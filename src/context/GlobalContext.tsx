c613fbab5343bdf50612c294ee88b75e1c222878
Dominik <d.madro57@gmail.com>
2020-07-30 18:37:48 +0200

    Dispatcher

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -6,8 +6,73 @@ export const GlobalContext = createContext<any>(null)
 
 const GlobalCtx: FC = ({ children }) => {
     const [authorization, setAuthorization] = useState(false)
+    const [token, setToken] = useState('')
+
+    const DISPATCH = async (type?: string, endpoint?: string, data?: any) => {
+        switch (type) {
+        case 'get': {
+            let result; let response;
+            try {
+                response = await axios.get(`${env}${endpoint}`, { 
+                    headers: { Authorization: `Bearer ${token}` } 
+                })
+                result = response.data
+            } catch (error) {
+                console.log(error);
+                result = null
+            }
+            return result
+        }
+
+        case 'post': {
+            let result; let response;
+            try {
+                response = await axios.post(`${env}${endpoint}`, data, { 
+                    headers: { Authorization: `Bearer ${token}` } 
+                })
+                result = response.data
+            } catch (error) {
+                console.log(error);
+                result = null
+            }
+            return result
+        }
+
+        case 'put': {
+            let result; let response;
+            try {
+                response = await axios.put(`${env}${endpoint}`, data, { 
+                    headers: { Authorization: `Bearer ${token}` } 
+                })
+                result = response.data
+            } catch (error) {
+                console.log(error);
+                result = null
+            }
+            return result
+        }
+
+        case 'delete': {
+            let result; let response;
+            try {
+                response = await axios.delete(`${env}${endpoint}`, { 
+                    headers: { Authorization: `Bearer ${token}` } 
+                })
+                result = response.data
+            } catch (error) {
+                console.log(error);
+                result = null
+            }
+            return result
+        }
+        
+        default:
+            break;
+        }
+    }
     
     const vProvider = {
+        DISPATCH,
         env,
         axios,
         authorization,
