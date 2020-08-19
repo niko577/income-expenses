ece7f329b0a5d168b0321b3e654d3554abcdd1c5
Dominik <d.madro57@gmail.com>
2020-08-19 21:26:46 +0200

    Deploy fix

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,1 @@
-const host = window.location.hostname
-let api = ''
-
-if (host === 'money.appjs.tk') {
-    api = 'https://api.money.appjs.tk/v0'
-} else {
-    api = 'https://api.dev-money.appjs.tk/v0'
-}
-
-export default api
+export default 'https://api.dev-money.appjs.tk/v0'
